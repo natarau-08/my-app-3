ba520b7afb969786101eb9e521a299d964338bac
Alexandru Ghita <alexandru.ghita138@gmail.com>
2025-02-15 14:49:30 +0200

    don't need this

--- lib/pages/reports/report_expense_tag_tracking_edit.dart
+++ lib/pages/reports/report_expense_tag_tracking_edit.dart
@@ -1,7 +1,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:my_app_3/app_secondary_page.dart';
-import 'package:my_app_3/forms/edit_tag_tracking_form.dart';
 
 class ReportExpenseTagTrackingEdit extends StatelessWidget{
   static const route = '/rep/ett/edit';
@@ -12,7 +11,7 @@ class ReportExpenseTagTrackingEdit extends StatelessWidget{
   Widget build(BuildContext context) {
     return AppSecondaryPage(
       title: 'Edit expense tag tracking',
-      child: EditTagTrackingForm()
+      child: Placeholder()
     );
   }
 }
